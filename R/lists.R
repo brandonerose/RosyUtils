4f8f91569980c6f902d2dd760321553c2f628570
brandonerose <brandon.ed.rose@gmail.com>
2024-02-25 16:30:01 -0500

    dev

---
+++ R/lists.R
@@ -0,0 +1,17 @@
+#' @title add_list_to_global
+#' @param x a named list
+#' @param only_dfs logical for only including data.frames in output
+#' @return environment adds
+#' @export
+add_list_to_global <- function(x,only_dfs=F){
+  if(!is.list(x))stop("x must be a list")
+  if(length(x)==0)stop("length(x) has to be at least 1")
+  if(class(x)=="character"){
+    if(is.null(names(x)))stop("if x is a character vector it must have names")
+    x<-as.list(x)
+  }
+  if(class(x)!="list")stop("x must be a named list or named character vector")
+  if(is.null(names(x)))stop("x list must have names")
+  if(only_dfs)x <- x[sapply(x,is.data.frame)]
+  list2env(x,envir = .GlobalEnv)
+}
