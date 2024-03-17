1a8afd20d2a2af5f418ff4c7c05c836e8da4d44c
brandonerose <73004123+brandonerose@users.noreply.github.com>
2024-03-17 15:33:13 -0400

    dev

--- R/other.R
+++ R/other.R
@@ -1,20 +1,30 @@
+#' @title sample1
 #' @export
 sample1 <- function(x){
   sample(x,1)
 }
+#' @title ul
 #' @export
 ul <- function(x){
   length(unique(x))
 }
+#' @title wl
 #' @export
 wl <- function(x){
   length(which(x))
 }
+#' @title dwl
 #' @export
 dwl <- function(x){
   length(which(duplicated(x)))
 }
+#' @title dw
 #' @export
 dw <- function(x){
   which(duplicated(x))
+}
+#' @title drop_nas
+#' @export
+drop_nas <- function(x) {
+  x[!sapply(x, is.na)]
 }
