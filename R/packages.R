a39bcd825cc061da1cf65c4abe32c44e2cb11385
brandonerose <brandon.ed.rose@gmail.com>
2024-03-30 10:22:02 -0400

    dev

--- R/packages.R
+++ R/packages.R
@@ -50,5 +50,14 @@ split_R_files <- function(source_dir= file.path(getwd(),"dev"), destination_dir=
     cat("File saved:", output_file, "\n")
   }
 }
+#' @title warn_or_stop
+#' @param m message character string
+#' @param warn_only logical for only warn
+#' @return message
+#' @export
+warn_or_stop <- function(m,warn_only=F){
+  if(warn_only)return(warning(m,immediate. = T))
+  return(stop(m))
+}
 
 
