10dcfc6f2ba29747bcce4f040e74b3d4fadd8f24
brandonerose <brandon.ed.rose@gmail.com>
2025-01-05 21:04:05 -0500

    dev

--- R/console.R
+++ R/console.R
@@ -1,6 +1,6 @@
 #' @title bullet_in_console
 #' @export
-bullet_in_console <- function(text = "",url = NULL,bullet_type = "i",collapse = T, file = NULL){
+bullet_in_console <- function(text = "",url = NULL,bullet_type = "i",collapse = T, file = NULL,verbosity=2){
   url_if <- ""
   file_if <- ""
   if(length(url)>0){
@@ -25,7 +25,13 @@ bullet_in_console <- function(text = "",url = NULL,bullet_type = "i",collapse = 
     }
     if(is.null(file_names))file_names <- file
     if(collapse)file_if <- paste0(file_if,collapse = " and ")
-    file_if <- paste0(" {cli::col_blue(cli::style_hyperlink('", file_names, "', 'file://", normalizePath(file), "'))}")
+    file_if <- paste0(
+      " {cli::col_blue(cli::style_hyperlink('",
+      sanitize_path(file_names),
+      "', '",
+      sanitize_path(paste0("file://",file)),
+      "'))}"
+    )
   }
   for(i in 1:length(url_if))text[i] <- paste0(text[i],url_if[i])
   for(i in 1:length(file_if))text[i] <- paste0(text[i],file_if[i])
